ed671f6a7163fe97e389c075beb3574435379f89
Yosoph <devyosoph@gmail.com>
2023-08-08 19:06:26 +0300

    Speech To Text

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -1,11 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 import 'package:voice_assistant/presentation/widgets/feature_box.dart';
 
 import '../core/app_colors.dart';
 import '../core/constants.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final speechToText = SpeechToText();
+  String lastWords = '';
+
+  @override
+  void initState() {
+    super.initState();
+    initSpeechToText();
+  }
+
+  Future<void> initSpeechToText() async {
+    await speechToText.initialize();
+    setState(() {});
+  }
+
+  Future<void> startListening() async {
+    await speechToText.listen(onResult: onSpeechResult);
+    setState(() {});
+  }
+
+  Future<void> stopListening() async {
+    await speechToText.stop();
+    setState(() {});
+  }
+
+  void onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = result.recognizedWords;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    speechToText.stop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,110 +67,121 @@ class HomePage extends StatelessWidget {
           Icons.menu,
         ),
       ),
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Center(
-                child: Container(
-                  height: 120,
-                  width: 120,
-                  margin: const EdgeInsets.only(
-                    top: 4,
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Center(
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    margin: const EdgeInsets.only(
+                      top: 4,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: AppColors.assistantCircleColor,
+                      shape: BoxShape.circle,
+                    ),
                   ),
+                ),
+                Container(
+                  height: 123,
                   decoration: const BoxDecoration(
-                    color: AppColors.assistantCircleColor,
                     shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/virtualAssistant.png'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              margin: const EdgeInsets.symmetric(
+                horizontal: 40,
+              ).copyWith(
+                top: 30,
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: AppColors.borderColor,
+                ),
+                borderRadius: BorderRadius.circular(20).copyWith(
+                  topLeft: Radius.zero,
+                ),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                ),
+                child: Text(
+                  'Good Morning, What task can i do for you?',
+                  style: TextStyle(
+                    color: AppColors.mainFontColor,
+                    fontSize: 25,
+                    fontFamily: Constants.fontFamily,
                   ),
                 ),
               ),
-              Container(
-                height: 123,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/virtualAssistant.png'),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10,
             ),
-            margin: const EdgeInsets.symmetric(
-              horizontal: 40,
-            ).copyWith(
-              top: 30,
-            ),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: AppColors.borderColor,
+            Container(
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.only(
+                top: 10,
+                left: 22,
               ),
-              borderRadius: BorderRadius.circular(20).copyWith(
-                topLeft: Radius.zero,
-              ),
-            ),
-            child: const Padding(
-              padding: EdgeInsets.symmetric(
-                vertical: 10.0,
-              ),
-              child: Text(
-                'Good Morning, What task can i do for you?',
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                'Here are a few features',
                 style: TextStyle(
-                  color: AppColors.mainFontColor,
-                  fontSize: 25,
                   fontFamily: Constants.fontFamily,
+                  color: AppColors.mainFontColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            margin: const EdgeInsets.only(
-              top: 10,
-              left: 22,
+            const Column(
+              children: [
+                FeatureBox(
+                  color: AppColors.firstSuggestionBoxColor,
+                  headerText: 'ChatGPT',
+                  descriptionText:
+                      'A smarter way to stay organized and informed with ChatGPT',
+                ),
+                FeatureBox(
+                  color: AppColors.secondSuggestionBoxColor,
+                  headerText: 'Dall-E',
+                  descriptionText:
+                      'Get inspired and stay creative with ypur personal assistant powered by Dall-E',
+                ),
+                FeatureBox(
+                  color: AppColors.thirdSuggestionBoxColor,
+                  headerText: 'Smart voice Assistant',
+                  descriptionText:
+                      'Get the best of both worlds with a voice assitant powered by Dall-E and ChatGPT',
+                ),
+              ],
             ),
-            alignment: Alignment.centerLeft,
-            child: const Text(
-              'Here are a few features',
-              style: TextStyle(
-                fontFamily: Constants.fontFamily,
-                color: AppColors.mainFontColor,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          const Column(
-            children: [
-              FeatureBox(
-                color: AppColors.firstSuggestionBoxColor,
-                headerText: 'ChatGPT',
-                descriptionText:
-                    'A smarter way to stay organized and informed with ChatGPT',
-              ),
-              FeatureBox(
-                color: AppColors.secondSuggestionBoxColor,
-                headerText: 'Dall-E',
-                descriptionText:
-                    'Get inspired and stay creative with ypur personal assistant powered by Dall-E',
-              ),
-              FeatureBox(
-                color: AppColors.thirdSuggestionBoxColor,
-                headerText: 'Smart voice Assistant',
-                descriptionText:
-                    'Get the best of both worlds with a voice assitant powered by Dall-E and ChatGPT',
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: AppColors.secondSuggestionBoxColor,
-        onPressed: () {},
+        onPressed: () async {
+          if(await speechToText.hasPermission && speechToText.isNotListening) {
+            await startListening();
+          } else if(speechToText.isListening) {
+            await stopListening();
+          } else {
+            await initSpeechToText();
+          }
+        },
         child: const Icon(
           Icons.mic,
           color: Colors.black,
